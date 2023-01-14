4bc46ca3d00eec7b46761934903bf9aa7ac102a4
BlindChickens <jacques@quantsolutions.co.za>
2023-01-14 20:03:57 +0200

    Fix missing values

--- miio/integrations/vacuum/dreame/dreamevacuum_miot.py
+++ miio/integrations/vacuum/dreame/dreamevacuum_miot.py
@@ -507,7 +507,7 @@ class DreameVacuum(MiotDevice, VacuumInterface):
 
         return DreameVacuumStatus(
             {
-                prop["did"]: prop["value"] if prop["code"] == 0 else None
+                prop["did"]: prop.get("value") if prop["code"] == 0 else None
                 for prop in self.get_properties_for_mapping(max_properties=10)
             },
             self.model,
